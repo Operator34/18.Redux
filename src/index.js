fc9e58cde79e90c690578131ac600ab6dbcab1b0
Andrey Loginov <operator34@yandex.ru>
2023-02-03 21:15:18 +0300

    18.1.7 Возврат к исходному коду

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { pipe, compose } from "lodash/fp";
 
 const App = (second) => {
-    const x = 2;
-    const double = (number) => number * 2;
-    const square = (number) => number * number;
-    const half = (number) => number / 2;
-    const mathCalculate = pipe(double, square, half);
-    const mathCalculate2 = compose(half, square, double);
-    return (
-        <h1>
-            {mathCalculate(x)} + {mathCalculate2(x)}
-        </h1>
-    );
+    return <h1>App</h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
