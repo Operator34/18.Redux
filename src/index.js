db3e6f4e23a9f5e2c1bcd6a0ea2ef854600f33c5
Andrey Loginov <operator34@yandex.ru>
2023-02-03 20:34:59 +0300

    18.1.6. Композиция функций

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { pipe, compose } from "lodash/fp";
 
 const App = (second) => {
-    return <h1>App</h1>;
+    const x = 2;
+    const double = (number) => number * 2;
+    const square = (number) => number * number;
+    const half = (number) => number / 2;
+    const mathCalculate = pipe(double, square, half);
+    const mathCalculate2 = compose(half, square, double);
+    return (
+        <h1>
+            {mathCalculate(x)} + {mathCalculate2(x)}
+        </h1>
+    );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
