5dbe5453d86b9c4a861913b73f0497f591767895
Andrey Loginov <operator34@yandex.ru>
2023-02-06 11:15:42 +0300

    18.1.14. Разработка Redux с нуля. Display Tasks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function createStore(initialState) {
+function taskReducer(state, action) {
+    // ф которая будет обрабатывать все действия с задачими
+    switch (action.type) {
+        case "task/completed":
+            const newArray = [...state];
+            const elementIndex = newArray.findIndex(
+                (el) => el.id === action.payload.id
+            );
+            newArray[elementIndex].completed = true;
+            return newArray;
+
+        default:
+            break;
+    }
+}
+
+function createStore(reducer, initialState) {
     // создаем стейт
     let state = initialState;
 
@@ -10,39 +26,43 @@ function createStore(initialState) {
     }
     function dispatch(action) {
         //изменение состояния стейта
-        console.log(action);
-        if (action.type === "task/completed") {
-            const newArray = [...state];
-            const elementIndex = newArray.findIndex(
-                (el) => el.id === action.payload.id
-            );
-            newArray[elementIndex].completed = true;
-            state = newArray;
-            console.log(state);
-        }
+        state = reducer(state, action);
     }
     return { getState, dispatch };
 }
 
-const store = createStore([
+const store = createStore(taskReducer, [
     { id: 1, description: "Task 1", completed: "false" },
+    { id: 2, description: "Task 2", completed: "false" },
 ]);
 const App = (second) => {
     console.log(store.getState()); //получаем состояние
     // store.dispatch({ type: "task/completed", payload: { id: 1 } }); //описываем сначало сущность потом действие, в тайп передаем что необходимо сделать, в пейлоад передаем все данные которые нам нелбходимы для того что найти этот объект в массиве и изменить его
+
+    const state = store.getState();
+    const completeTask = (taskId) => {
+        store.dispatch({
+            type: "task/completed",
+            payload: { id: taskId },
+        });
+        console.log(store.getState());
+    };
     return (
         <>
             <h1>App</h1>
-            <button
-                onClick={() =>
-                    store.dispatch({
-                        type: "task/completed",
-                        payload: { id: 1 },
-                    })
-                }
-            >
-                complete
-            </button>
+
+            <ul>
+                {state.map((el) => (
+                    <li key={el.id}>
+                        <p>{el.description}</p>
+                        <p>{` Completed: ${el.completed}`}</p>
+                        <button onClick={() => completeTask(el.id)}>
+                            complete
+                        </button>
+                        <hr />
+                    </li>
+                ))}
+            </ul>
         </>
     );
 };
