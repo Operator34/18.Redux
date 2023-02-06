f8f0f63981d7c6a1e18c38c17a6f5ab91828c5e2
Andrey Loginov <operator34@yandex.ru>
2023-02-06 10:21:05 +0300

    18.1.12. Разработка Redux с нуля. Dispatch

--- src/index.js
+++ src/index.js
@@ -2,20 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function createStore(initialState) {
+    // создаем стейт
     let state = initialState;
 
     function getState() {
         return state;
     }
-    return { getState };
+    function dispatch(action) {
+        //изменение состояния стейта
+        console.log(action);
+        if (action.type === "task/completed") {
+            const newArray = [...state];
+            const elementIndex = newArray.findIndex(
+                (el) => el.id === action.payload.id
+            );
+            newArray[elementIndex].completed = true;
+            state = newArray;
+            console.log(state);
+        }
+    }
+    return { getState, dispatch };
 }
 
 const store = createStore([
     { id: 1, description: "Task 1", completed: "false" },
 ]);
 const App = (second) => {
-    console.log(store.getState());
-    return <h1>App</h1>;
+    console.log(store.getState()); //получаем состояние
+    // store.dispatch({ type: "task/completed", payload: { id: 1 } }); //описываем сначало сущность потом действие, в тайп передаем что необходимо сделать, в пейлоад передаем все данные которые нам нелбходимы для того что найти этот объект в массиве и изменить его
+    return (
+        <>
+            <h1>App</h1>
+            <button
+                onClick={() =>
+                    store.dispatch({
+                        type: "task/completed",
+                        payload: { id: 1 },
+                    })
+                }
+            >
+                complete
+            </button>
+        </>
+    );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
